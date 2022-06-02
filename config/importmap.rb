02c5a720c6ac02ad6b7c52ac33484dd7c602dbe3
charlie <charliecasabona@gmail.com>
2022-06-02 07:47:29 -0400

    update import maps

--- config/importmap.rb
+++ config/importmap.rb
@@ -2,16 +2,17 @@
 
 pin "application", preload: true
 
-pin "@hotwired/turbo-rails", to: "@hotwired--turbo-rails.js" # @7.1.3
-pin "@hotwired/turbo", to: "@hotwired--turbo.js" # @7.1.0
-pin "@rails/actioncable/src", to: "@rails--actioncable--src.js" # @7.0.3
+pin "@hotwired/turbo-rails", to: "https://ga.jspm.io/npm:@hotwired/turbo-rails@7.1.3/app/javascript/turbo/index.js"
+pin "@hotwired/turbo", to: "https://ga.jspm.io/npm:@hotwired/turbo@7.1.0/dist/turbo.es2017-esm.js"
+pin "@rails/actioncable/src", to: "https://ga.jspm.io/npm:@rails/actioncable@7.0.3/src/index.js"
 
-# ./bin/importmap pin '@rails/actiontext' --download
+# ./bin/importmap pin '@rails/actiontext'
 pin '@rails/actiontext', to: 'actiontext.js'
 pin '@rails/activestorage', to: '@rails--activestorage.js'
 
-# ./bin/importmap pin 'trix' --download
+# ./bin/importmap pin 'trix'
 pin 'trix' # @2.0.0
-pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
-pin "trix"
-pin "@rails/actiontext", to: "actiontext.js"
+pin "@hotwired/turbo-rails", to: "https://ga.jspm.io/npm:@hotwired/turbo-rails@7.1.3/app/javascript/turbo/index.js"
+pin "trix", to: "https://ga.jspm.io/npm:trix@2.0.0-beta.0/dist/trix.js"
+pin "@rails/actiontext", to: "https://ga.jspm.io/npm:@rails/actiontext@6.0.5/app/javascript/actiontext/index.js"
+pin "@rails/activestorage", to: "https://ga.jspm.io/npm:@rails/activestorage@6.1.6/app/assets/javascripts/activestorage.js"
